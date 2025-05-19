fc330a5b1d0d34590b4a1e69d1c56bd779218b52
Jeremias Kuehne <jeremias.kuehne@protonmail.com>
2025-05-19 13:04:03 +0200

    Small menu adjustments

---
+++ dyspraxia-and-the-dragon/EXP (Experimental Stuff)/EXP Scripts/UI/bbcode_main_menu.gd
@@ -0,0 +1,10 @@
+@tool
+extends RichTextEffect
+class_name RichTextMinusCurve
+
+var bbcode := "-curve"
+
+func _process_custom_fx(char_fx):
+	# Mapping index to give seed between 0 and 90
+	var seed = deg_to_rad((char_fx.relative_index + 0.5) * 180 / 6)
+	char_fx.offset += Vector2(0, sin(seed) * 30)
