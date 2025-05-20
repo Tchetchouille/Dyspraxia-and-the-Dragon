9d39d661d7c41989c0f93ff2bf112d5aa35d854f
Jeremias Kuehne <jeremias.kuehne@protonmail.com>
2025-05-21 00:01:14 +0200

    added controller controls to credits

---
+++ dyspraxia-and-the-dragon/EXP (Experimental Stuff)/EXP Scripts/UI/credits.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_anything_pressed() and is_visible_in_tree():
+		var main_menu_scene = load("res://EXP (Experimental Stuff)/EXP Scenes/UI/main_menu.tscn").instantiate()
+		get_tree().root.add_child(main_menu_scene)
+		queue_free()
--- dyspraxia-and-the-dragon/EXP (Experimental Stuff)/EXP Scripts/UI/main_menu.gd
+++ dyspraxia-and-the-dragon/EXP (Experimental Stuff)/EXP Scripts/UI/main_menu.gd
@@ -1,8 +1,4 @@
 extends Control
-
-func _ready() -> void:
-	$"../Music".stream = load("res://Assets/Musics/Professor and the Plant.mp3")
-	$"../Music".play()	
 	
 func _on_mode_histoire_pressed() -> void:
 	var story_scene = load("res://EXP (Experimental Stuff)/EXP Scenes/UI/story.tscn").instantiate()
@@ -15,4 +11,6 @@ func _on_quitter_pressed() -> void:
 
 
 func _on_crédits_pressed() -> void:
-	$Credits.visible = true
+	var credit_scene = load("res://EXP (Experimental Stuff)/EXP Scenes/UI/credits.tscn").instantiate()
+	get_tree().root.add_child(credit_scene)
+	queue_free()
