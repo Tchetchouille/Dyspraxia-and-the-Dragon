611cf83a37c56461aff9f61fab1ddaf7dfaccf0a
Jeremias Kuehne <jeremias.kuehne@protonmail.com>
2025-05-20 22:37:24 +0200

    Added main menu pointer interpolation

--- dyspraxia-and-the-dragon/EXP (Experimental Stuff)/EXP Scripts/UI/main_menu_pointer.gd
+++ dyspraxia-and-the-dragon/EXP (Experimental Stuff)/EXP Scripts/UI/main_menu_pointer.gd
@@ -1,5 +1,18 @@
 extends Control
+
 var target
+var speed = 8.0
+@onready var button1 = $"../Title/VBoxContainer/Buttons/HBoxContainer/Mode Histoire"
+
+
+func _ready() -> void:
+	target = button1.position
+	get_viewport().gui_focus_changed.connect(_on_focus_changed)
 
 func _process(delta: float) -> void:
-	global_position = global_position.lerp(target, 0.01)
+	position = self.position.lerp(target, delta * speed)
+
+func _on_focus_changed(control: Control) -> void:
+	if control != null:
+		target = control.position
+		print(target)
