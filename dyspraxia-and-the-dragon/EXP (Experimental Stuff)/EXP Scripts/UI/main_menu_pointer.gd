55366c9a028615c996b7cbd5fef0d0dec1721acf
Jeremias Kuehne <jeremias.kuehne@protonmail.com>
2025-05-20 22:00:41 +0200

    Added focus to main menu

--- dyspraxia-and-the-dragon/EXP (Experimental Stuff)/EXP Scripts/UI/main_menu_pointer.gd
+++ dyspraxia-and-the-dragon/EXP (Experimental Stuff)/EXP Scripts/UI/main_menu_pointer.gd
@@ -3,7 +3,3 @@ var target
 
 func _process(delta: float) -> void:
 	global_position = global_position.lerp(target, 0.01)
-
-func _on_mode_histoire_focus_entered() -> void:
-	target = $"../Title/VBoxContainer/Buttons/HBoxContainer/Mode Histoire".position
-	position = position.lerp(target, 1)
