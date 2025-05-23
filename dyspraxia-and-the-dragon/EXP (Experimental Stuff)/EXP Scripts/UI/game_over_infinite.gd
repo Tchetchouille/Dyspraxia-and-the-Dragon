71f3dbc3d7444d35119232eb0dcd19c9564b27c6
Jeremias Kuehne <jeremias.kuehne@protonmail.com>
2025-05-23 08:05:39 +0200

    Fixed infinite mode replay glitch
    Game over from infinite mode used to incorrectly bring the player back to the history mode.

---
+++ dyspraxia-and-the-dragon/EXP (Experimental Stuff)/EXP Scripts/UI/game_over_infinite.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var listening = false
+
+func _process(_delta: float) -> void:
+	if Input.is_anything_pressed() and listening:
+		var dyspraxia_fight_scene = load("res://EXP (Experimental Stuff)/EXP Scenes/infinite_mode.tscn").instantiate()
+		get_tree().root.add_child(dyspraxia_fight_scene)
+		queue_free()
+
+
+func _on_timer_timeout() -> void:
+	listening = true
