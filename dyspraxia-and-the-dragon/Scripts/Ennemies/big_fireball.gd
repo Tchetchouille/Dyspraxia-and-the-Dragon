8aada3194f9f86f58c331a901bc5d8af5493d0d6
Jeremias Kuehne <jeremias.kuehne@protonmail.com>
2025-07-15 14:25:29 +0200

    Reorganized scenes and scripts

--- dyspraxia-and-the-dragon/Scripts/Ennemies/big_fireball.gd
+++ dyspraxia-and-the-dragon/Scripts/Ennemies/big_fireball.gd
@@ -1,8 +1,17 @@
 extends RigidBody2D
 class_name UpFireball
 
-var size = Vector2(2, 2)
+
+
 var dmg = 6
+var fireball_impulse = 300
+var gravity = 300
+var size = Vector2(2, 2)
+var target_offset = 0.1
+var impulse_offset = 200
+
+
+
 @onready var player = $"../PlayerCharacter"
 @onready var fireball_spawn = $"../Dragon/UpFireballSpawn"
 @onready var particles = preload("res://Scenes/Ennemies/particles_system.tscn")
@@ -27,27 +36,27 @@ var dmg = 6
 	preload("res://Assets/Fireball Pictures/r3.png"),
 	preload("res://Assets/Fireball Pictures/r4.png")
 ]
+# Indexes of the current fireball sprite for each color.
 var y_index = 0
 var o_index = 0
 var r_index = 0
-
-@export var fireball_impulse = 300
-var gravity = 300
-var target_offset = 0.1
-var impulse_offset = 200
 var is_reflected = false
 # Used to store the gravity scale
 # So the fireball can start with no gravity and aim for the player more precisely
-var original_gravity_scale
 var frame_count = 0
 
+
+
+# When the fireball is created, we set its properties and apply an initial impulse.
 func _ready() -> void:
-	original_gravity_scale = gravity_scale
+#	Setting gravity to 0 for more pecise aiming
 	gravity_scale = 0
 	var spawn_point = fireball_spawn.global_position
 	position = spawn_point
-	apply_impulse(Vector2(-130, -fireball_impulse))
 	scale = size
+	apply_impulse(Vector2(-130, -fireball_impulse))
+
+
 
 func _physics_process(_delta: float) -> void:
 	scale = size
@@ -57,6 +66,8 @@ func _physics_process(_delta: float) -> void:
 	look_at(global_position - linear_velocity)
 	_flicker(frame_count, 9, 3)
 	frame_count += 1
+
+
 
 func _on_body_entered(_body: Node) -> void:
 	if is_reflected == false:
